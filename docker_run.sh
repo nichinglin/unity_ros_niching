80df4a6c1052e3e69477ee36dfe761facec03a2e
nichinglin <nclin94@gmail.com>
2019-03-05 09:11:04 +0800

    update docker run

--- docker_run.sh
+++ docker_run.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env bash
+export repo=haptic_device_niching
+
 if [ "$1" == "same" ]; then
-  docker exec -it unity_ros bash
+  docker exec -it $repo bash
 else
-  docker run --name unity_ros --rm -it --net=host --privileged \
+  docker run --name $repo --rm -it --net=host --privileged \
              -v /dev:/dev \
              -v /etc/localtime:/etc/localtime:ro \
              -v /var/run/docker.sock:/var/run/docker.sock \
-             -v /home/$USER/unity_ros_niching:/root/unity_ros_niching \
+             -v /home/$USER/$repo:/root/$repo \
              -v /home/$USER/.bashrc:/root/.bashrc \
-             unity_ros_niching:laptop
+             nichinglin/$repo:laptop
 fi
